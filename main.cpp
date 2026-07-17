fd7320b567ced2246bda3a02277ffc4fa3c8f237
suacayipkisi <konuki8523@gmail.com>
2026-07-17 06:10:13 +0300

    added a bug and a few  littlechanges

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <ostream>
 
-enum Planets:int{
+enum class Planets : int{
     mercury,
     venus,
     earth,
@@ -18,20 +18,18 @@ enum Planets:int{
 std::ostream& operator<<(std::ostream& os, Planets planet){
     
     switch (planet) {
-        case mercury: return os << "Mercury";
-        case venus: return os << "Venus";
-        case earth: return os << "Earth";
-        case mars: return os << "Mars";
-        case jupiter: return os << "Jupiter";
-        case saturn: return os << "Saturn";
-        case uranus: return os << "Uranus";
-        case neptune: return os << "Neptune";
-        case pluto: return os << "Pluto";
-        case unknown: return os << "Unknown";
+        case Planets::mercury: return os << "Mercury";
+        case Planets::venus: return os << "Venus";
+        case Planets::earth: return os << "Earth";
+        case Planets::mars: return os << "Mars";
+        case Planets::jupiter: return os << "Jupiter";
+        case Planets::saturn: return os << "Saturn";
+        case Planets::uranus: return os << "Uranus";
+        case Planets::neptune: return os << "Neptune";
+        case Planets::pluto: return os << "Pluto";
+        case Planets::unknown: return os << "Unknown";
         default: return os << "Error!";
     }
-    os << "Error!";
-    return os;
 }
 
 int main(){
@@ -39,8 +37,8 @@ int main(){
     std::cout << "\n";
     int planetNum{};
     while(true){
-        Planets currentPlanet{planetNum};
-        if(currentPlanet == unknown){
+        Planets currentPlanet = static_cast<Planets>(planetNum);
+        if(currentPlanet == Planets::unknown){
             break;
         }
         std::cout << "Hello, " << currentPlanet << "!" << std::endl;
